72c7650edeb24441fc1e04034a911fd7a93f6e78
Yash Sehgal <yashsehgal.work@gmail.com>
2022-03-29 17:53:36 +0000

    feat: profile setup page content added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import AuthenticationScreen from "./screens/AuthenticationScreen";
 import Header from "./components/Header";
+import ProfileCustomization from "./screens/ProfileCustomization";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<AuthenticationScreen />} />
         <Route path="/auth" element={<AuthenticationScreen />} />
+        <Route path="/create-profile" element={<ProfileCustomization />} />
       </Routes>
     </div>
   )
--- src/screens/AuthenticationScreen.js
+++ src/screens/AuthenticationScreen.js
@@ -2,13 +2,13 @@
 export default function AuthenticationScreen() {
     return (
         <div className="authentication-screen-wrapper flex flex-row items-center justify-center">
-            <div className="auth-content-wrapper mt-24 p-12">
+            <div className="auth-content-wrapper mt-32 p-12">
                 <h1 className="text-2xl leading-snug font-semibold text-slate-800 text-center">
                 Hope you'll find your partner today!
                 </h1>
                 <div className="auth-buttons-wrapper flex flex-col gap-4 items-start mt-6">
-                    <button className="auth_twitter_button w-full h-auto rounded-md py-2 bg-pink-600 leading-snug font-semibold text-white">Continue with Twitter</button>
-                    <button className="auth_twitter_button w-full h-auto rounded-md py-2 bg-white leading-snug font-semibold text-pink-600 border border-pink-600">Continue with GitHub</button>
+                    <button className="auth_twitter_button w-full h-auto rounded-md py-2 bg-pink-600 hover:bg-pink-700 leading-snug font-semibold text-white">Continue with Twitter</button>
+                    <button className="auth_github_button w-full h-auto rounded-md py-2 bg-white hover:bg-pink-100 leading-snug font-semibold text-pink-600 border border-pink-600">Continue with GitHub</button>
                 </div>
             </div>
         </div>
