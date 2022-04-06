bd2e8f032f00eee47d1610307696fc28cc60f8b6
Yash Sehgal <yashsehgal.work@gmail.com>
2022-04-06 19:56:46 +0000

    pushing some stuff

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import AuthenticationScreen from "./screens/AuthenticationScreen";
 import Header from "./components/Header";
 import ProfileCustomization from "./screens/ProfileCustomization";
+import Landing from "./screens/Landing";
 
 export default function App() {
   return (
     <div className="App screen-container">
-      <Header type={true} />
+      <Header type={false} />
       <Routes>
-        <Route path="/" element={<AuthenticationScreen />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<AuthenticationScreen />} />
         <Route path="/create-profile" element={<ProfileCustomization />} />
       </Routes>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
     "./src/**/**/*.{js,jsx,ts,tsx}",
     "./src/*.{js,jsx,ts,tsx}",
+    "./public/*.{html, htm}"
   ],
   theme: {
     extend: {},
