2652782870e800e8a64157e223b6ed9d300e6677
Yash Sehgal <yashsehgal.work@gmail.com>
2022-04-28 14:29:50 +0000

    Header component modified

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import { FaTimes } from 'react-icons/fa';
+
+import ReactModal from 'react-modal';
+
+ReactModal.setAppElement('#root');
+
 export default function Header({HEADER_STATE}) {
     const solidButton = "leading-snug text-white px-4 py-2 font-semibold gradient rounded-md flex flex-row items-center justify-center";
     const secondaryButton = "leading-snug text-white px-4 py-2 font-semibold bg-white bg-opacity-50 hover:bg-opacity-60 rounded-md flex flex-row items-center justify-center";
+    
+    const [getStartedButtonModalRef, setGetStartedButtonModal] = useState(false);
+
     const [headerOptionsRef] = useState([
         { 
             type: 'link', 
@@ -22,24 +31,24 @@ export default function Header({HEADER_STATE}) {
                 location: 'https://github.com/yashsehgal/devumble'
             }
         },
-        {
-            type: 'button',
-            content: 'Login',
-            redirect: {
-                typeOf: 'route',
-                location: '/login'
-            },
-            buttonType: 'secondary'
-        },
-        {
-            type: 'button',
-            content: 'Join Today',
-            redirect: {
-                typeOf: 'route',
-                location: '/create-account'
-            },
-            buttonType: 'solid'
-        }
+        // {
+        //     type: 'button',
+        //     content: 'Login',
+        //     redirect: {
+        //         typeOf: 'route',
+        //         location: '/login'
+        //     },
+        //     buttonType: 'secondary'
+        // },
+        // {
+        //     type: 'button',
+        //     content: 'Get Started',
+        //     redirect: {
+        //         typeOf: 'route',
+        //         location: '/create-account'
+        //     },
+        //     buttonType: 'solid'
+        // }
     ]);
     if (HEADER_STATE === 'logged-in') {
         return (
@@ -108,10 +117,57 @@ export default function Header({HEADER_STATE}) {
                                         )
                                     }
                                 })}
+                                <li className='header-option-item font-product text-sm'>
+                                    <button className={solidButton}
+                                        onClick={() => setGetStartedButtonModal(true)}
+                                    >
+                                        Get Started
+                                    </button>
+                                </li>
                             </ul>
                         </div>
                     </div>
                 </div>
+                
+                {/* React Modals Dump */}
+                <ReactModal
+                    isOpen={getStartedButtonModalRef}
+                    onRequestClose={() => setGetStartedButtonModal(false)}
+                    style={{
+                        overlay: {
+                            background: 'rgba(0, 0, 0, 0.500)',
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                        },
+                        content: {
+                            backgroundColor: '#121212',
+                            color: 'white',
+                            boxShadow: '0px 0px 6px 12px rgba(0, 0, 0, 0.225)',
+                            border: 'transparent',
+                            height: 'fit-content',
+                            width: '640px',
+                            borderRadius: '8px',
+                            padding: '28px',
+                            marginRight: 'auto',
+                            marginLeft: 'auto',
+                            marginTop: 'auto',
+                            marginBottom: 'auto'
+                        }
+                    }}
+                >
+                    <div className="close-button-layer w-full h-auto flex flex-row items-center justify-between">
+                        <h3 className="leading-snug font-product text-white text-lg">Getting Started with devumble</h3>
+                        <button className="p-2 flex flex-row items-center justify-center text-white rounded-md bg-transparent hover:bg-gray-600 hover:bg-opacity-60"
+                            onClick={() => setGetStartedButtonModal(false)}
+                        >
+                            <FaTimes />
+                        </button>
+                    </div>
+                    
+                </ReactModal>
+
             </React.Fragment>    
         )
     }
--- app/src/screens/Landing.jsx
+++ app/src/screens/Landing.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { getUserData } from '../api-middleware/github-api';
 
+import { FaGithub, FaArrowRight } from 'react-icons/fa'
+
 export default function Landing() {
     return (
         <React.Fragment>
@@ -10,19 +12,20 @@ export default function Landing() {
                         start your <span className="gradient-text">journey</span> to find your pair programming <span className="gradient-text">partner</span>
                     </h1>
                     <div className='github-auth-cta-section-wrapper mt-12 flex flex-row items-center justify-between w-fit mx-auto'>
-                        <div className="github-username-input-component-wrapper px-3 py-2 rounded-full bg-white bg-opacity-30 w-fit flex flex-row items-center gap-3 mx-auto">
-                            <span className="github-placeholder-wrapper leading-snug font-semibold text-lg text-slate-400 px-1">
-                                @
+                        <div className="github-username-input-component-wrapper px-2 py-2 rounded-full bg-white bg-opacity-30 w-fit flex flex-row items-center gap-3 mx-auto">
+                            <span className="github-placeholder-wrapper flex flex-row gap-1 items-center leading-snug font-semibold text-lg text-slate-400 px-2">
+                                <FaGithub />
                             </span>
                             <input type="text" placeholder='GitHub Username'
                                 className='bg-transparent focus:border-0 focus:outline-none focus:border-transparent text-white w-[260px]'
                             />
-                            <button className='px-4 py-2 gradient text-white flex flex-row items-center justify-center font-normal rounded-full'>
-                                Continue
+                            <button className='px-4 py-2 gradient text-white flex flex-row items-center justify-center gap-2 font-normal rounded-full'>
+                                Continue <FaArrowRight />
                             </button>
                         </div>
                         <hr className='translate -rotate-45 w-24' />
-                        <button className='px-5 py-3 gradient text-white flex flex-row items-center justify-center font-semibold rounded-md'>
+                        <button className='px-5 py-3 gradient text-white flex flex-row items-center justify-center gap-2 font-semibold rounded-md'>
+                            <FaGithub />
                             Authenticate with GitHub
                         </button>
                     </div>
