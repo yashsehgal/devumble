ecef03f520bf0e52014cdb261692b3305104b483
Yash Sehgal <yashsehgal.work@gmail.com>
2022-04-28 16:19:22 +0000

    Landing Page CTA section hanged

--- app/src/screens/Landing.jsx
+++ app/src/screens/Landing.jsx
@@ -11,7 +11,7 @@ export default function Landing() {
                     <h1 className='font-product text-white leading-snug font-bold text-5xl text-center w-[24ch] mx-auto'>
                         start your <span className="gradient-text">journey</span> to find your pair programming <span className="gradient-text">partner</span>
                     </h1>
-                    <div className='github-auth-cta-section-wrapper mt-12 flex flex-row items-center justify-between w-fit mx-auto'>
+                    <div className='github-auth-cta-section-wrapper mt-12 flex flex-col items-stretch justify-between gap-4 w-fit mx-auto'>
                         <div className="github-username-input-component-wrapper px-2 py-2 rounded-full bg-white bg-opacity-30 w-fit flex flex-row items-center gap-3 mx-auto">
                             <span className="github-placeholder-wrapper flex flex-row gap-1 items-center leading-snug font-semibold text-lg text-slate-400 px-2">
                                 <FaGithub />
@@ -23,7 +23,11 @@ export default function Landing() {
                                 Continue <FaArrowRight />
                             </button>
                         </div>
-                        <hr className='translate -rotate-45 w-24' />
+                        <div className="section-divider-wrapper flex flex-row items-center gap-2 w-auto h-auto ">
+                            <div className='w-full h-[1px] bg-white opacity-30' />
+                            <span className='text-white opacity-30 font-normal text-sm'>OR</span>
+                            <div className='w-full h-[1px] bg-white opacity-30' />
+                        </div>
                         <button className='px-5 py-3 gradient text-white flex flex-row items-center justify-center gap-2 font-semibold rounded-md'>
                             <FaGithub />
                             Authenticate with GitHub
