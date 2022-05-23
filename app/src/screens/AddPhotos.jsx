7380b97f2132a939061e1e4cf52d74be11ca535c
Yash Sehgal <yashsehgal.work@gmail.com>
2022-05-23 15:30:04 +0000

    feat: image upload ui added

--- app/src/screens/AddPhotos.jsx
+++ app/src/screens/AddPhotos.jsx
@@ -13,6 +13,16 @@ export default function AddPhotos() {
                 </h1>
                 <p className="text-center text-sm text-gray-400 text-opacity-60 leading-snug">Adding a photo will make sure that people will see the actual YOU. Not your github profile. Get some attention and add a photo</p>
             </div>
+            <div className="image-uploading-section-wrapper mt-12 w-fit h-fit mx-auto flex flex-roe items-center gap-6">
+                <div className="w-32 h-40 rounded-md border border-dashed border-gray-500 text-gray-500 flex flex-row items-center justify-center hover:border-gray-300 hover:text-gray-300 cursor-pointer">
+                    Add Photo
+                </div>
+                <div className="w-32 h-40 rounded-md border border-dashed border-gray-500 text-gray-500 flex flex-row items-center justify-center hover:border-gray-300 hover:text-gray-300 cursor-pointer">
+                    Add Photo
+                </div>
+            </div>
+            <div className="mt-8" />
+            <button className="p-2 text-gray-500 font-normal text-sm block mx-auto hover:text-white">Skip the step</button>
         </div>
     )
 }
