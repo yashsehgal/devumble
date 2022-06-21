8f66baeb4b914d86378e5931cf8a449e11a03405
Yash Sehgal <yashsehgal.work@gmail.com>
2022-06-21 18:36:34 +0000

    feat: swipe functionality added

--- app/src/screens/Explore.jsx
+++ app/src/screens/Explore.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { createDemoUserData } from "../utils/user-db";
 
+import TinderCard from 'react-tinder-card';
+
 // let userDataRef = [];
 // userDataRef = createDemoUserData().then(res=> {
 //     userDataRef = res;
@@ -19,6 +21,11 @@ export default function Explore() {
             console.log('api error-log', err);
         });
     }, []);
+
+    const onCardSwipe = function(direction) {
+        console.log('swiped in direction: ', direction);
+    }
+
     return (
         <React.Fragment>
             <div className="explore-page-wrapper screen-container">
@@ -28,27 +35,37 @@ export default function Explore() {
                         These recommendations are generated according to your profile
                     </p>
                 </div>
-                <div className="recommendations-section-container mt-12">
-                    <div className="w-fit h-auto mx-auto">
+                <div className="recommendations-section-container mt-12 w-fit mx-auto">
+                    <div className="relative flex flex-row items-stretch">
                         {userDataRef.length!==0 ? userDataRef.map((recommenedUser, userIndex) => {
                             let recommendingUser = recommenedUser.data;
                             return (
-                                <div className="recommended-user-card px-5 pr-24 pt-6 pb-3 flex flex-row items-start transition-all rounded-md bg-white text-black" 
-                                    key={userIndex}
-                                >
-                                    <div className="recommended-user-content-wrapper">
-                                        <img src={recommendingUser.avatar_url} 
-                                            alt={`${recommendingUser.login}-avatar`} 
-                                            className="w-24 h-24 rounded-full mx-auto"
-                                        />
-                                        <h1 className="leading-snug text-lg text-black font-semibold mt-2">{recommendingUser.login}</h1>
-                                        {
-                                            recommendingUser.location 
-                                                ? <p className="leading-snug text-sm text-black">from {recommendingUser.location}</p>
-                                                : <React.Fragment></React.Fragment>
-                                        }
+                                <TinderCard onSwipe={onCardSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['up', 'down']} key={userIndex}>
+                                    <div className="recommended-user-card absolute w-[420px] h-[400px] px-5 pr-24 pt-6 pb-3 flex flex-row items-start transition-all rounded-md bg-white text-black" >
+                                        <div className="recommended-user-content-wrapper">
+                                            {
+                                                recommendingUser.login.toString().toLowerCase() === 'yashsehgal'
+                                                    ? <p className="leading-snug text-sm px-4 py-1 font-semibold text-white bg-purple-900 w-fit h-fit rounded-full">The God</p>
+                                                    : <React.Fragment></React.Fragment>
+                                            }
+                                            <img src={recommendingUser.avatar_url} 
+                                                alt={`${recommendingUser.login}-avatar`} 
+                                                className="w-24 h-24 rounded-full mx-auto"
+                                            />
+                                            <h1 className="leading-snug text-lg text-black font-semibold mt-2">{recommendingUser.login}</h1>
+                                            {
+                                                recommendingUser.location 
+                                                    ? <p className="leading-snug text-sm text-black">from {recommendingUser.location}</p>
+                                                    : <React.Fragment></React.Fragment>
+                                            }
+                                            {
+                                                recommendingUser.login.toString().toLowerCase() === 'yashsehgal'
+                                                    ? <p className="leading-snug text-sm font-normal text-gray-500">You don't have to swipe anywhere. The god has already been matched to your profile</p>
+                                                    : <React.Fragment></React.Fragment>
+                                            }
+                                        </div>
                                     </div>
-                                </div>
+                                </TinderCard>
                                 )
                             }) : <React.Fragment>
                                     <h1 className="leading-snug text-center text-white text-opacity-60 font-semibold mt-8 text-lg">
